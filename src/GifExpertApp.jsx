fa8b3f12a5042b2e7947e2c78a1a3df9342e7e59
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2023-01-01 18:45:40 -0500

    Incorporando useState

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react"
 
 export const GifExpertApp = () => {
-  return (
-    <>
-        {/* titulo */}
-        <h1>GifExpertApp</h1>
 
-        {/* Input */}
+    const [categories, setCategories] = useState(['One Punch','Dragon Ball']);
 
-        {/* Listado de Gif */}
-            {/* Gif Item */}
-            
-    </>
-  )
+    console.log(categories);
+
+    return (
+        <>
+            {/* titulo */}
+            <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gif */}
+            <ol>
+                { categories.map( category => { return <li key={category}>{category}</li>} )}
+                
+            </ol>
+                {/* Gif Item */}
+
+        </>
+    )
 }
