8e2128437cf3a5eebd408a5540f29abb42a14b0a
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2023-01-09 22:35:04 -0500

    Incorporando UseEffect

--- src/GifGrid.jsx
+++ src/GifGrid.jsx
@@ -1,14 +1,22 @@
 import {getGifs} from './helpers/getGifs'
+import {useState, useEffect} from 'react'
 
 export const GifGrid = ({category}) => {
 
+    const [counter, setCounter] = useState(10)
 
-    getGifs(category);
+    useEffect( ()=>{
+        getGifs(category);
+    }, [] )
+
+    
 
     return (
         <>
             <h3>{category}</h3>
             
+            <h5>{counter}</h5>
+            <button onClick={()=>setCounter(counter + 1)}>+1</button>
         </>
     )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=yo2T0bQ44kMurR5wju0sIjZJWGRD8M8E&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=yo2T0bQ44kMurR5wju0sIjZJWGRD8M8E&q=${category}&limit=10`;
     const resp = await fetch(url);
     const {data} = await resp.json();
 
