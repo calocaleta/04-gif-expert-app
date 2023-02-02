c8dc6bca0750de59ac2cee655daea435f71cc73c
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2023-02-01 23:41:55 -0500

    Haciendo Mock de un custom hook

--- test/components/GifGrid.test.jsx
+++ test/components/GifGrid.test.jsx
@@ -1,9 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs');
+
 
 describe('Pruebas en <GifGrid/>',() => {
     const category='One Punch';
     test('debe de mostrar el loading inicialmente',()=> {
+
+        useFetchGifs.mockReturnValue({
+            images:[],
+            isLoading: true
+        });
+
         render(<GifGrid category={category} />);
         //screen.debug();
         expect(screen.getByText('Cargando...'));
@@ -11,7 +21,27 @@ describe('Pruebas en <GifGrid/>',() => {
     });
 
     test('debe de mostrar items cuando se cargan las imágenes useFetchGifs',()=>{
+        const gifs = [
+            {
+                id:'ABC',
+                title: 'Saitama',
+                url: 'https://localhost/saitama.jpg'
+            },
+            {
+                id:'Goku',
+                title: 'Goku',
+                url: 'https://localhost/goku.jpg'
+            },
+        ];
         
+        useFetchGifs.mockReturnValue({
+            images:[],
+            isLoading: false
+        });
+
+        render(<GifGrid category={category} />);
+        //screen.debug();
+        expect(screen.getAllByRole('img').lenght).toBe(2);
     });
 
 });
