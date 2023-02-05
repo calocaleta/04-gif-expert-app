6d8439ae01a8d08f970239d0d270fe3f3d012664
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2023-02-04 23:20:08 -0500

    Pruebas con custom Hooks 2/2

--- test/hooks/useFetchGifs.test.js
+++ test/hooks/useFetchGifs.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { useFetchGifs } from "../../src/hooks/useFetchGifs";
 
 describe('Pruebas en el hook useFetchGifs', ()=>{
@@ -9,6 +9,20 @@ describe('Pruebas en el hook useFetchGifs', ()=>{
 
         expect(images.length).toBe(0);
         expect(isLoading).toBeTruthy();
+        
+        //useFetchGifs();
+    });
+
+    test('debe de retornar un arreglo de imagenes y isLoading en false',async () => {
+        const {result} = renderHook(()=>useFetchGifs('One Punch'));
+
+        await waitFor(
+            () => expect(result.current.images.length).toBeGreaterThan(0)
+        );
+        const {images,isLoading}=result.current;
+
+        expect(images.length).toBeGreaterThan(0);
+        expect(isLoading).toBeFalsy();
         
         //useFetchGifs();
     });
